3f30ad4ac9340c34332ca2401489349125a13af1
mattcce <ce.mattheus.cheong@gmail.com>
2025-09-21 16:17:27 +0800

    Add remark command

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,59 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's remark in the remark book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidRemark(String)}
+ */
+public class Remark {
+
+    public static final String MESSAGE_CONSTRAINTS = "Remarks can take any values, and it should not be blank";
+
+    public final String value;
+
+    /**
+     * Constructs an {@code remark}.
+     *
+     * @param remark A valid remark.
+     */
+    public Remark(String remark) {
+        requireNonNull(remark);
+        checkArgument(isValidRemark(remark), MESSAGE_CONSTRAINTS);
+        value = remark;
+    }
+
+    /**
+     * Returns true if a given string is a valid remark (always).
+     */
+    public static boolean isValidRemark(String test) {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Remark)) {
+            return false;
+        }
+
+        Remark otherremark = (Remark) other;
+        return value.equals(otherremark.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+}
